4865631ea09ec8805416b64d2d75fc59697086b7
TigranGhazaryan <tigran.ghazaryan@outlook.com>
2022-01-16 19:48:27 +0400

    updates to Vector.hpp

--- Vector.hpp
+++ Vector.hpp
@@ -41,19 +41,43 @@ public:
     class Iterator
     {
     public:
-		typedef std::forward_iterator_tag iterator_category;
+        typedef std::forward_iterator_tag iterator_category;
 		typedef std::ptrdiff_t difference_type;
 		typedef T value_type;
 		typedef value_type* pointer;
 		typedef value_type& reference;
     public:
-        Iterator (pointer ptr) { this->m_ptr = ptr;}
-        reference operator*()const { return *this->m_ptr; }
-        pointer operator->() { return this->m_ptr; }
-        Iterator& operator++(){ this->m_ptr++; return *this;}
-        Iterator operator++(int) { Iterator temp = *this; ++(*this); return temp;}
-        friend bool operator==(const Iterator& a, const Iterator& b) { return a.m_ptr == b.m_ptr; }
-        friend bool operator!=(const Iterator& a, const Iterator& b) { return a.m_ptr != b.m_ptr; }
+        Iterator (pointer ptr)
+        { 
+            this->m_ptr = ptr;
+        }
+        reference operator*()const 
+        { 
+            return *this->m_ptr; 
+        }
+        pointer operator->() 
+        { 
+            return this->m_ptr; 
+        }
+        Iterator& operator++()
+        { 
+            this->m_ptr++; 
+            return *this;
+        }
+        Iterator operator++(int) 
+        { 
+            Iterator temp = *this; 
+            ++(*this); 
+            return temp;
+        }
+        friend bool operator==(const Iterator& a, const Iterator& b) 
+        { 
+            return a.m_ptr == b.m_ptr; 
+        }
+        friend bool operator!=(const Iterator& a, const Iterator& b)
+        { 
+            return a.m_ptr != b.m_ptr; 
+        }
     private:
         pointer m_ptr;
 
